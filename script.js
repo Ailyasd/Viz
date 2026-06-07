660dbe9c5d03d1d66e8610cd8636ed06bd75e11e
Ailyasd <154557496+Ailyasd@users.noreply.github.com>
2026-06-07 20:56:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+const navButtons = document.querySelectorAll(".promo__nav-btn");
+const backButtons = document.querySelectorAll(".info-card__back-btn");
+const panes = document.querySelectorAll(".info-card__pane");
+const infoCardWindow = document.getElementById("info-card-window");
+
+const tabButtons = document.querySelectorAll(".contacts-tabs__btn");
+const contactPanes = document.querySelectorAll(".contacts-pane");
+
+function switchPane(targetPaneId, expandCard) {
+  const logoWrapper = document.querySelector(".promo__logo-wrapper");
+
+  panes.forEach((pane) => pane.classList.remove("info-card__pane--active"));
+
+  if (expandCard) {
+    infoCardWindow.classList.add("info-card--expanded");
+
+    if (logoWrapper && window.innerWidth < 768) {
+      logoWrapper.classList.add("is-hidden");
+    }
+  } else {
+    infoCardWindow.classList.remove("info-card--expanded");
+
+    if (logoWrapper) {
+      logoWrapper.classList.remove("is-hidden");
+    }
+  }
+
+  setTimeout(() => {
+    const targetPane = document.getElementById(targetPaneId);
+    if (targetPane) targetPane.classList.add("info-card__pane--active");
+  }, 150);
+}
+
+navButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const targetTab = button.getAttribute("data-tab");
+
+    navButtons.forEach((btn) => btn.classList.remove("promo__nav-btn--active"));
+    button.classList.add("promo__nav-btn--active");
+
+    switchPane(`pane-${targetTab}`, true);
+  });
+});
+
+backButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    navButtons.forEach((btn) => btn.classList.remove("promo__nav-btn--active"));
+    switchPane("pane-default", false);
+  });
+});
+
+tabButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const targetSubtab = button.getAttribute("data-subtab");
+    const targetPane = document.getElementById(`subtab-${targetSubtab}`);
+
+    tabButtons.forEach((btn) =>
+      btn.classList.remove("contacts-tabs__btn--active")
+    );
+    contactPanes.forEach((pane) => {
+      pane.classList.remove("contacts-pane--active");
+    });
+
+    button.classList.add("contacts-tabs__btn--active");
+    if (targetPane) {
+      targetPane.classList.add("contacts-pane--active");
+    }
+  });
+});
